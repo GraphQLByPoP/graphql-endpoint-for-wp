41adb45a08cb707e0f2168b813b4ffa15d23a924
Leonardo Losoviz <leo@getpop.org>
2020-06-15 15:53:03 +0800

    Initial commit

---
+++ src/Component.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\GraphQLEndpointForWP;
+
+use PoP\Root\Component\AbstractComponent;
+use PoP\Root\Component\YAMLServicesTrait;
+use PoP\ComponentModel\Container\ContainerBuilderUtils;
+use PoP\ComponentModel\Facades\Instances\InstanceManagerFacade;
+
+/**
+ * Initialize component
+ */
+class Component extends AbstractComponent
+{
+    use YAMLServicesTrait;
+
+    // const VERSION = '0.1.0';
+
+    public static function getDependedComponentClasses(): array
+    {
+        return [
+            \PoP\APIEndpointsForWP\Component::class,
+            \PoP\GraphQLAPI\Component::class,
+        ];
+    }
+
+    /**
+     * Initialize services
+     */
+    protected static function doInitialize(
+        array $configuration = [],
+        bool $skipSchema = false,
+        array $skipSchemaComponentClasses = []
+    ): void {
+        parent::doInitialize($configuration, $skipSchema, $skipSchemaComponentClasses);
+        ComponentConfiguration::setConfiguration($configuration);
+        self::initYAMLServices(dirname(__DIR__));
+    }
+
+    /**
+     * Boot component
+     *
+     * @return void
+     */
+    public static function beforeBoot(): void
+    {
+        parent::beforeBoot();
+
+        // Initialize services
+        $instanceManager = InstanceManagerFacade::getInstance();
+        $endpointHandlerServiceClasses = ContainerBuilderUtils::getServiceClassesUnderNamespace(__NAMESPACE__ . '\\EndpointHandlers');
+        foreach ($endpointHandlerServiceClasses as $serviceClass) {
+            $instanceManager->getInstance($serviceClass)->initialize();
+        }
+    }
+}
